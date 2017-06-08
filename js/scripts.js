be91e973aa6b1be912f26f0edec5f110d546aa0d
JStein92 <jonathanstein@live.com>
2017-06-08 13:18:12 -0700

    add input boxes and basic animation to quiz section, as well as a submit button for the form

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,19 @@
 
 $(function(){ //document is ready
 
+  $('#question1 .hawaii').click(function(){
+    //alert(this.className);
+    $("#question1 .hawaii input").prop("checked", true)
+  });
+  $('#question1 .other').click(function(){
+    //alert(this.className);
+    $("#question1 .other input").prop("checked", true)
+  });
+
+  $('form#quiz').submit(function(event){
+    //submit answers, if all are checked then show results, if not show warning
+    event.preventDefault();
+    alert('test');
+  });
 
 });
