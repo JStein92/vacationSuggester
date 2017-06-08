4126a97a31b00feb4e0cf408cc7c5253246bd4eb
JStein92 <jonathanstein@live.com>
2017-06-08 14:41:31 -0700

    change some logic in how points are given to quiz answers

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,44 @@
 
 $(function(){ //document is ready
 
+//create point variables to "score" user's choices
+var hawaiiPoints = 0;
+var egyptPoints = 0;
+var texasPoints = 0;
+var disneyworldPoints = 0;
+var antarcticaPoints = 0;
+var homePoints = 0;
+
   $('#question1 .hawaii').click(function(){
-    //alert(this.className);
     $("#question1 .hawaii input").prop("checked", true)
   });
-  $('#question1 .other').click(function(){
-    //alert(this.className);
-    $("#question1 .other input").prop("checked", true)
+  $('#question1 .egypt').click(function(){
+    $("#question1 .egypt input").prop("checked", true)
   });
+  $('#question1 .texas').click(function(){
+    $("#question1 .texas input").prop("checked", true)
+  });
+  $('#question1 .disneyworld').click(function(){
+    $("#question1 .disneyworld input").prop("checked", true)
+  });
+  $('#question1 .antarctica').click(function(){
+    $("#question1 .antarctica input").prop("checked", true)
+  });
+  $('#question1 .home').click(function(){
+    $("#question1 .home input").prop("checked", true)
+  });
+
 
   $('form#quiz').submit(function(event){
     //submit answers, if all are checked then show results, if not show warning
     event.preventDefault();
-    alert('test');
+    for (var i = 1; i < 30; i++) {
+
+      if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'hawaii'){ //cycle through every question
+        hawaiiPoints++;
+      }
+    }
+    alert(hawaiiPoints);
   });
 
 });
