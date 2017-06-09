405b8a0172d55f0ae473c20b550b7a406178946d
JStein92 <jonathanstein@live.com>
2017-06-09 09:05:50 -0700

    change the logic of checking boxes to cut down on extra code, moved the scoring to a backend function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
 //backend logic
-
-$(function(){ //document is ready
-
-//create point variables to "score" user's choices
 var hawaiiPoints = 0;
 var egyptPoints = 0;
 var texasPoints = 0;
@@ -10,36 +6,66 @@ var disneyworldPoints = 0;
 var antarcticaPoints = 0;
 var homePoints = 0;
 
-  $('#question1 .hawaii').click(function(){
-    $("#question1 .hawaii input").prop("checked", true)
+function checkValues(){
+  for (var i = 1; i <= 5; i++) { // go through the 5 questions
+
+    if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'hawaii'){ //if the checked box's
+      hawaiiPoints++;
+    } else if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'egypt'){
+      egyptPoints++;
+    } else if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'texas'){
+      texasPoints++;
+    } else if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'disneyworld'){
+      disneyworldPoints++;
+    } else if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'antarctica') {
+      antarcticaPoints++;
+    } else if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'home') {
+      homePoints++;
+    } else{
+      alert('checked box for ' + 'question'+i+' has no value or question' + i + ' is not found')
+    }
+  }
+}
+
+$(function(){ //document is ready
+
+//create point variables to "score" user's choices
+
+
+  $('#question1 figure').click(function(){
+    $('input',this).prop("checked", true)
   });
-  $('#question1 .egypt').click(function(){
-    $("#question1 .egypt input").prop("checked", true)
+  $('#question2 figure').click(function(){
+    $('input',this).prop("checked", true)
   });
-  $('#question1 .texas').click(function(){
-    $("#question1 .texas input").prop("checked", true)
+  $('#question3 figure').click(function(){
+    $('input',this).prop("checked", true)
   });
-  $('#question1 .disneyworld').click(function(){
-    $("#question1 .disneyworld input").prop("checked", true)
+  $('#question4 figure').click(function(){
+    $('input',this).prop("checked", true)
   });
-  $('#question1 .antarctica').click(function(){
-    $("#question1 .antarctica input").prop("checked", true)
+  $('#question5 figure').click(function(){
+    $('input',this).prop("checked", true)
   });
-  $('#question1 .home').click(function(){
-    $("#question1 .home input").prop("checked", true)
-  });
+
+
+
 
 
   $('form#quiz').submit(function(event){
-    //submit answers, if all are checked then show results, if not show warning
-    event.preventDefault();
-    for (var i = 1; i < 30; i++) {
+    //reset scores
+    hawaiiPoints = 0;
+    egyptPoints = 0;
+    texasPoints = 0;
+    disneyworldPoints = 0;
+    antarcticaPoints = 0;
+    homePoints = 0;
 
-      if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'hawaii'){ //cycle through every question
-        hawaiiPoints++;
-      }
-    }
-    alert(hawaiiPoints);
+    event.preventDefault();
+    checkValues(); // call function to check which values are "checked" by user and add points accordingly
+
+    alert('hawaii: ' + hawaiiPoints + ' egypt: ' + egyptPoints + ' texas: ' + texasPoints + ' disneyworld: ' +
+  disneyworldPoints + ' antarctica: ' + antarcticaPoints + ' home:' + homePoints);
   });
 
 });
