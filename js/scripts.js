bfb16f96a9fdfbb32d4b70f161e3ffda0ff6069c
JStein92 <jonathanstein@live.com>
2017-06-10 11:44:06 -0700

    add images to all quiz answers and finish logic to calculate score and show results

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,46 @@
 //backend logic
-var hawaiiPoints = 0;
-var egyptPoints = 0;
-var texasPoints = 0;
-var disneyworldPoints = 0;
-var antarcticaPoints = 0;
-var homePoints = 0;
+var hawaiiPoints = {
+  score: 0,
+  name:'hawaii'
+}
+var egyptPoints = {
+  score: 0,
+  name:'egypt'
+}
+var texasPoints = {
+  score: 0,
+  name:'texas'
+}
+var disneyworldPoints = {
+  score: 0,
+  name:'disney world'
+}
+var antarcticaPoints = {
+  score:0,
+  name:'antarctica'
+}
+var homePoints = {
+  score: 0,
+  name:'home'
+}
 
 function checkValues(){
   for (var i = 1; i <= 5; i++) { // go through the 5 questions
 
-    if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'hawaii'){ //if the checked box's
-    hawaiiPoints++;
-  } else if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'egypt'){
-    egyptPoints++;
-  } else if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'texas'){
-    texasPoints++;
-  } else if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'disneyworld'){
-    disneyworldPoints++;
-  } else if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'antarctica') {
-    antarcticaPoints++;
-  } else if ( $("input[name='question"+ i +"'"+"]:checked").val() == 'home') {
-    homePoints++;
+    if ( $("input[name='question"+ i +"'"+"]:checked").val() === 'hawaii'){ //if the checked box's
+    hawaiiPoints.score++;
+  } else if ( $("input[name='question"+ i +"'"+"]:checked").val() === 'egypt'){
+    egyptPoints.score++;
+  } else if ( $("input[name='question"+ i +"'"+"]:checked").val() === 'texas'){
+    texasPoints.score++;
+  } else if ( $("input[name='question"+ i +"'"+"]:checked").val() === 'disneyworld'){
+    disneyworldPoints.score++;
+  } else if ( $("input[name='question"+ i +"'"+"]:checked").val() === 'antarctica') {
+    antarcticaPoints.score++;
+  } else if ( $("input[name='question"+ i +"'"+"]:checked").val() === 'home') {
+    homePoints.score++;
   } else{
-    //  alert('checked box for ' + 'question'+i+' has no value or question' + i + ' is not found')
+    //    alert('checked box for ' + 'question'+i+' has no value or question' + i + ' is not found')
   }
 }
 }
@@ -50,28 +68,51 @@ $(function(){ //document is ready
 
   $('.col-md-4').hover(function(){
     this.style.borderColor="#70a6ff";
-    //  this.style.borderColor='black';
   });
   $('.col-md-4').mouseleave(function(){
     this.style.borderColor="#a3c6ff";
-    //  this.style.borderColor='black';
   });
 
   $('form#quiz').submit(function(event){
-    //reset scores
-    hawaiiPoints = 0;
-    egyptPoints = 0;
-    texasPoints = 0;
-    disneyworldPoints = 0;
-    antarcticaPoints = 0;
-    homePoints = 0;
-
     event.preventDefault();
-    checkValues(); // call function to check which values are "checked" by user and add points accordingly
-    $('.result').slideDown("slow");
-    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-    //   alert('hawaii: ' + hawaiiPoints + ' egypt: ' + egyptPoints + ' texas: ' + texasPoints + ' disneyworld: ' +
-    // disneyworldPoints + ' antarctica: ' + antarcticaPoints + ' home:' + homePoints);
-  });
 
-});
+    //reset scores
+    hawaiiPoints.score=0;
+    egyptPoints.score = 0;
+    texasPoints.score = 0;
+    disneyworldPoints.score = 0;
+    antarcticaPoints.score = 0;
+    homePoints.score = 0;
+
+    checkValues();     // call function to check which values are "checked" by user and add points accordingly
+
+    //check to make sure max points are reached (I'm sure there's a better way)
+    if ((hawaiiPoints.score+egyptPoints.score+texasPoints.score+disneyworldPoints.score+antarcticaPoints.score+homePoints.score)<5){
+      alert('Please answer all the questions!');
+    } else {
+      $('#destination').text('');
+
+      //find max score
+      var finalDestination = Math.max(hawaiiPoints.score, egyptPoints.score, texasPoints.score,
+        disneyworldPoints.score, antarcticaPoints.score, homePoints.score);
+
+        //put objs in an array,check which ones have the max score, add name value to destination ID string
+        var scoreArray = [hawaiiPoints,egyptPoints,texasPoints,disneyworldPoints,antarcticaPoints,homePoints];
+        var destCount = 0;
+        var destinationString = '';
+        for (var i = 0; i < scoreArray.length; i++) {
+          if (scoreArray[i].score === finalDestination){
+            destinationString += (scoreArray[i].name.toUpperCase() + " or ");
+          }
+        }
+        //remove the extra " or "
+        destinationString = destinationString.toString().substring(0, destinationString.length-4)
+
+        $('#destination').text(destinationString); //add destination string to ID
+        $('.result').slideDown("slow"); // show results
+        $("html, body").animate({ scrollTop: $(document).height() }, 1000); //slide to  bottom of page
+      }
+
+    });
+    
+  });
